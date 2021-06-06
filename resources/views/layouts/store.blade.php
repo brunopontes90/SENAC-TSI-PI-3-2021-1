1a468af735f68492c9e7dc7f7eb75fb53a1c6a3e
r820 <44514586+r820@users.noreply.github.com>
2021-06-06 12:58:18 -0300

    bugs2

--- resources/views/layouts/store.blade.php
+++ resources/views/layouts/store.blade.php
@@ -1,16 +1,23 @@
-{{-- <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+   <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous"> -->
+
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+
+   <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script> -->
     <script src="https://kit.fontawesome.com/8455a3d02b.js" crossoringin="anonymous"></script>
+    @yield('css')
 </head>
-<body> --}}
-    {{-- <header>
+<body>
+    <header>
         <nav class="navbar navbar-expand-sm navbar-light bg-white shadow-sm">
             <div class="container">
-              <h1><a class="navbar-brand" href="{{url('/')}}">Loja do Senac</a></h1>
+              <h1><a class="navbar-brand" href="{{url('/')}}">Loja do Games</a></h1>
               <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
               </button>
@@ -33,7 +40,19 @@
                     </ul>
                   </li>
                 </ul>
-                <div class="navbar-nav ms-auto">
+
+
+                {{-- EDITAR O MENU DE BUSCA --}}
+                <form class="d-flex ms-2" action="{{ route('search') }}">
+                    @csrf
+                    <input class="form-control" type="text" name="search" id="search">
+                    <button class="ms-2 btn btn-secondary" style="min-width:95px"><i class="fas fa-search"></i>Buscar</button>
+                </form>
+
+
+                <!--
+                {{--
+                <div class="navbar-nav ">
                     @if (Auth()->user())
                         <span class="nav-link">{{ Auth()->user()->name}}</span>
                         <a class="nav-link" href="{{ route('cart.show') }}">Carrinho ({{  \App\Models\Cart::count() }})</a>
@@ -42,12 +61,65 @@
                         <a class="nav-link" href="{{ route('login') }}">Logar</a>
                     @endif
                 </div>
+                --}}
+
+
+
+                <div class="navbar-nav ms-auto">
+
+                    {{-- SE ESTIVER LOGADO, RETORNA NOME, CARRINHO, QTD E LOGOUT --}}
+                    @if (Auth()->user())
+                    {{-- SAUDAÇÃO COM NOME --}}
+                        <span class="nav-link text-BLACK">Olá, {{ Auth()->user()->name}}</span>
+                        {{-- LOGO CARRINHO + QTD --}}
+                        <a class="nav-link text-white" href="{{ route('cart.show') }}">
+                            <i class="fas fa-shopping-cart"></i>
+                            ({{\App\Models\Cart::count()}})
+                        </a>
+
+                        {{-- FAZ LOGOUT --}}
+                        <form class="d-flex" method="POST" action="{{ route('logout') }}">
+                            @csrf
+                            <button class="btn"><i class="fas fa-sign-in-alt text-white"></i>
+                            <i class="fas fa-sign-out-alt"></i>
+                            </button>
+                        </form>
+                    @else
+                        {{-- SE NÃO TIVER LOGADO, OPÇÃO DE REGISTRAR OU LOGAR --}}
+                        <a class="nav-link text-white" href="{{ route('register') }}">Registrar</a>
+                        <a class="nav-link text-white" href="{{ route('login') }}"><i class="fas fa-user"></i></a>
+                    @endif
+                    -->
+
+                <div class="navbar-nav ms-auto">
+                @if (Auth()->user())
+                    <span class="nav-link">Olá, {{Auth()->user()->name}}</span>
+                    <a class="nav-link" href="{{ route('cart.show') }}">Carrinho ({{  \App\Models\Cart::count() }})</a>
+                    <form method="POST" action="{{route('logout')}}">
+                     @csrf
+                        <button type="submit" class="btn btn-primary bg-dark text-white">
+                            <i class="fas fa-sign-out-alt"></i>
+                        </button>
+                    </form>
+                @else
+                {{-- BOTAO LOGIN --}}
+                    <nav class="justify-content-end">
+                        <a class="fas fa-user text-white" href="{{route('login')}}">
+                            <h2 class="d-none">Login</h2>
+                        </a>
+                    </nav>
+                @endif
+                </div>
+
+
+
+                </div>
               </div>
             </div>
-          </nav> -- }}
-    </header> --}}
+          </nav>
+        </header>
 
-    {{-- <main class="container my-4 text-center">
+    <main class="container my-4 text-center">
 
         @if(@session()->has('success'))
             <div class="alert alert-success" role="alert">{{ session()->get('success') }}</div>
@@ -57,9 +129,9 @@
             <div class="alert alert-danger" role="alert">{{ session()->get('error') }}</div>
         @endif
         @yield('content')
-    </main> --}}
+    </main>
 
-    {{-- <footer class="container bg-primary text-white p-5">
+    <footer class="container bg-primary text-white p-5">
         <div class="row">
             <div class="col-6">
                 <h2>Localização:</h2>
@@ -67,7 +139,7 @@
                     Rua Lorem, ipsum dolor.<br>
                     Lorem, ipsum. Lorem, LR<br>
                     CEP: 00000-000<br>
-                    Telefone: (11) 99999-9999
+                    Telefone: (11) 99999-99998
                 </address>
             </div>
             <div class="col-6">
@@ -77,7 +149,7 @@
                     <li>Sabado: 10:00h as 16:00h</li>
                 </ul>
             </div>
-        </div> --}}
-    {{-- </footer>
+        </div>
+    </footer>
 </body>
-</html> --}}
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,9 @@
 @extends('layouts.store')
 @section('css')
+
+
 <style>
-    #banner{
+    #banner {
         background: url('https://via.placeholder.com/2000x800');
         background-size: cover;
         background-repeat: no-repeat;
@@ -12,11 +14,12 @@
 @endsection
 
 @section('content')
-<section id="banner" class="d-flex alingn-items-center p-4">
+
+<section id="banner" class="d-flex align-items-center p-4">
     <div>
-        <span class="h2 d-block text-captalize mb-0">Toda loja aqui</span>
+        <span class="h2 d-block text-capitalize mb-0">Toda loja aqui</span>
         <span class="h1 d-block text-uppercase fw-bold mb-3">Promoções</span>
-        <button class="btn btn- lg btn-primary">Veja nosso produtos</button>
+        <button class="btn btn-lg btn-primary">Veja nosso produtos</button>
     </div>
 </section>
 <section>
@@ -29,19 +32,22 @@
     <div class="row">
         @foreach(\App\Models\Product::promocoes() as $product)
             <div class="col-lg-4 col-md-6 col-sm-10">
-                <div class="text-center" style="height= 250px">
-                    <img src="{{ asset($product->image)}}" class="h-100">
+                <div class="text-center" style="height: 230px;">
+                    <img src="{{ asset($product->image)}}" class="h-100" style="height: 200px">
                 </div>
                 <div class="text-center">
                     <span class="d-block fw-bold">{{$product->name}}</span>
                     <span class="d-block">R${{$product->price}}</span>
                     <div class="mt-2">
                         <a href="{{ route('product.show', $product->id) }}" class="btn btn-secondary">Visualizar</a>
-                        <a href="{{ route('cart.add', $product->id) }}" class="btn-primary">Adicionar carrinho</a>
+                        <a href="{{ route('cart.add', $product->id) }}" class="btn btn-primary">Adicionar carrinho</a>
                 </div>
                 </div>
             </div>
         @endforeach
     </div>
-</section
+</section>
+
+{{-- @include('layouts.store') --}}
+{{-- @include('layouts.footer') --}}
 @endsection
